74f4800912b0039aed91ead004a0418086ff3311
inteelbr@hotmail.com <inteelbr@hotmail.com>
2023-10-21 22:51:19 -0300

    16 - Validação Da Forma Correta

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -2,9 +2,17 @@
 <hr>
 <a href="{{ route('supports.index') }}">Listagem</a>
 <hr>
+@if($errors->any())
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+<hr>
 <form action="{{ route('support.store') }}" method="post">
     @csrf
-    <input type="text" class="text" placeholder="Assunto" name="subject">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+    <input type="text" class="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}">
+    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
     <button type="submit">Criar</button>
-</form>
+</form>Request
