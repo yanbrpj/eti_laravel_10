6ca3f02ee996643ac3051b5d195aa7bbf76c1d71
inteelbr@hotmail.com <inteelbr@hotmail.com>
2023-10-21 21:41:19 -0300

    10 - Listagem de Dados

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,20 @@
-<h1>SUPORTES</h1>
+<h1>Lista de Suportes</h1>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Body</th>
+        <th>Ações</th>
+    </thead>
+    <tbody>
+        @foreach($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>AÇÔES</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
